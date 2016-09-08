0d1db09b6bdb50ee8ed009f6bc828a96e6620644
XaElink <xaelink@qq.com>
2016-09-08 18:15:21 +0800

    食品列表模拟百分之七十

---
+++ app/common/Loading.js
@@ -0,0 +1,75 @@
+/**
+ * Created by chenjunjun
+ * 加载框
+ */
+
+import React, {Component} from 'react';
+import{
+    ActivityIndicator,
+    StyleSheet,
+    View,
+    Text
+} from 'react-native';
+
+
+class Loading extends React.Component {
+    // 构造
+    constructor(props) {
+        super(props);
+        // 初始状态
+        this.state = {
+            //默认加载
+            loading: true
+        };
+    }
+
+    // /**
+    //  * 开启一个定时器
+    //  */
+    // componentDidMount() {
+    //     //setState()方法重新刷新界面改变loading值
+    //     this.timer = setTimeout(() => {
+    //             this.setState({loading: false})
+    //         },
+    //         8000)
+    // }
+
+    // /**
+    //  * 清除定时器 一定要清除
+    //  */
+    // componentWillUnMount() {
+    //     this.timer && clearTimeout(this.timer)
+    // }
+
+
+    /**
+     *
+     * @returns {XML}
+     */
+    render() {
+        return (
+            <View style={styles.loadViewStyle}>
+            <ActivityIndicator
+                //加载状态大小 samll / large
+                size='large'
+                //加载状态的颜色
+                color='red'
+                //是否显示
+                animating={true}
+            />
+                <Text>加载中...</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    loadViewStyle:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'white'
+    }
+});
+
+export default Loading
